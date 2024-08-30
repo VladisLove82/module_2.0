9c800100502051ce95d12d1dc624456c965f3509
Vlad <zloe.moloco@gmail.com>
2024-08-30 21:22:37 +0300

    Исправил условия

--- module_2/module_2_2.py
+++ module_2/module_2_2.py
@@ -3,7 +3,7 @@ second = 456
 third = 789
 if first==second==third:
     print(3)
-if first!=second!=third:
+elif first!=second!=third:
     print(0)
 else:
     print()
@@ -15,7 +15,7 @@ if first==second:
     print(0)
 if second==third:
     print(0)
-if first==third:
+elif first==third:
     print(2)
 else:
     print()
