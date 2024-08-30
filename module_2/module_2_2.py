a0380e8746345146e85934bd1e0aaf8b6368c885
Vlad <zloe.moloco@gmail.com>
2024-08-30 21:19:45 +0300

    new zadacha

---
+++ module_2/module_2_2.py
@@ -0,0 +1,21 @@
+first = 123
+second = 456
+third = 789
+if first==second==third:
+    print(3)
+if first!=second!=third:
+    print(0)
+else:
+    print()
+
+first = 42
+second = 69
+third = 42
+if first==second:
+    print(0)
+if second==third:
+    print(0)
+if first==third:
+    print(2)
+else:
+    print()
