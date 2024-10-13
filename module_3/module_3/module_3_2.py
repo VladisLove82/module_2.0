3e04a877d627198f1108ba7330da46c2ab5af973
Vlad <zloe.moloco@gmail.com>
2024-10-13 16:12:48 +0300

    Задача "Рассылка писем"

--- module_3/module_3/module_3_2.py
+++ module_3/module_3/module_3_2.py
@@ -1,7 +1,7 @@
 def send_email(message, recipient, *, sender="university.help@gmail.com"):
-    domain_list = ["@", ".com", ".ru", ".net"]
 
-    if recipient not in domain_list and sender not in domain_list:
+    if "@" not in recipient or not recipient.endswith((".com", ".ru", ".net")) \
+            or "@" not in sender or not sender.endswith((".com", ".ru", ".net")):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
 
     elif sender is recipient:
