bbd11b5ccda65695da0fd3f8ef2f7acb638f95cd
Vlad <zloe.moloco@gmail.com>
2024-10-24 21:56:21 +0300

    Задача "Рекурсивное умножение цифр"

--- module_3/module_3/module_3_5.py
+++ module_3/module_3/module_3_5.py
@@ -1,12 +1,23 @@
 def get_multiplied_digits(number):
+    if number == 0:
+        return 0
+
     str_number = str(number)
     first = int(str_number[0])
 
-    if len(str_number) > 1:
+    if first == 0:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    if len(str_number) > 2:
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
         return first
 
 
-result = get_multiplied_digits(40203)
-print(result)
+result1 = get_multiplied_digits(40203)
+print(result1)
+
+result2 = get_multiplied_digits(0)
+print(result2)
+
+result3 = get_multiplied_digits(4020)
+print(result3)
